d943c013ff430b6883fd3e5b4753e7dec73fabc1
Cassio De Bolba <44919506+cassiobolba@users.noreply.github.com>
2022-05-01 22:09:18 +0200

    add weather function

--- Docker/python-sample-app/my-app.py
+++ Docker/python-sample-app/my-app.py
@@ -1,2 +1,18 @@
+import requests
+
+def get_weather_info():
+    
+    city = "Porto Alegre"
+    
+    api_key = "868a26a88dcad371f4205a319f26be8c"
+
+    url = "http://api.openweathermap.org/data/2.5/weather?q="+ str(city) +"&appid="+ api_key
+
+    json_data = requests.get(url).json()
+    
+    return print(f"Current Temperature for {str(city)} is {json_data['main']['temp']/10}")
+
+
 if __name__ == "__main__":
-    print("Cassio Lindo")
+
+    get_weather_info()
